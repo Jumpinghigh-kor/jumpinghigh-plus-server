91c5d815e366a336b8cba9ecf9cdb8e5c0953649
Jumpinghigh-kor <jumpingkor@naver.com>
2026-01-06 09:17:53 +0900

    2026.01.06 09:17

--- src/update-log-app/update-log-app.controller.ts
+++ src/update-log-app/update-log-app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { UpdateLogAppService } from './update-log-app.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UpdateLogAppInfo } from './dto/update-log-app.dto';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('update-log-app')
 @UseGuards(JwtAuthGuard)
@@ -9,6 +10,7 @@ export class UpdateLogAppController {
   constructor(private readonly updateLogAppService: UpdateLogAppService) {}
 
   @Post('getUpdateLogAppInfo')
+  @Public()
   async getUpdateLogAppInfo(): Promise<{ success: boolean; data: UpdateLogAppInfo[] | null; code: string }> {
     return this.updateLogAppService.getUpdateLogAppInfo();
   }
